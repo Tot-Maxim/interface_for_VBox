fc7bc2c578cba65f7b36e35cf870c17073c2e7ad
oem <boom.rezonans@yandex.ru>
2024-05-03 14:26:25 +0300

    доработка, добавление чтения

--- tes-write-tjs.py
+++ tes-write-tjs.py
@@ -7,7 +7,7 @@ import json
 #    json_data = json.load(file)
 
 packets = []
-packet = Ether(dst="0a:1a:de:3c:f0:5d", src = "0a:1a:de:3c:f0:5d") / IP(dst = "192.168.1.1", src= "192.168.1.1") / TCP(dport = 12345, sport = 54321) / Raw(load = 'Test text from VBOX')
-    
+packet = Ether(dst="0a:1a:de:3c:f0:5d", src = "0a:1a:de:3c:f0:5d") / IP(dst = "192.168.1.1", src= "192.168.1.1") / TCP(dport = 12345, sport = 54321) / Raw(load = 'Test meme text from VBOX')
+
 # Отправка пакета на интерфейс TAP
 sendp(packet, iface="tap0")
