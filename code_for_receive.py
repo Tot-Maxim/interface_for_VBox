42495f4bd42d38f8a8a47d5f416fd6711b1de0aa
serverside <boom.rezonans@yandex.ru>
2024-05-07 22:37:47 +0300

    update

--- code_for_receive.py
+++ code_for_receive.py
@@ -1,28 +1,24 @@
+import time
 from array import array
 import fcntl
 import os
 import struct
 import subprocess
-import tempfile
-import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
-# Заданные переменные
-source_port = 0x0a1a
-destination_port = 0xde3c
-sequence_number = 0xf05d0a1a
-acknowledgment_number = 0xde3cf05d
-flags = 0x800
-checksum = 0x6f7
-state = 1
 current_dir = '/media/sf_FilePack'
-path_dir = os.path.join(current_dir, 'data_file.docx')
 
-# Некоторые константы, используемые для ioctl файла устройства. Я получил их с помощью простой программы на Cи
+# Некоторые константы
 TUNSETIFF = 0x400454ca
 TUNSETOWNER = TUNSETIFF + 2
 IFF_TUN = 0x0001
 IFF_TAP = 0x0002
 IFF_NO_PI = 0x1000
+state = 1
+temp_read = b''
+temp_write = b''
+ast_read_time = 0
 
 # Открытие файла, соответствующего устройству TUN, в двоичном режиме чтения/записи без буферизации.
 tun = open('/dev/net/tun', 'r+b', buffering=0)
@@ -32,6 +28,14 @@ fcntl.ioctl(tun, TUNSETOWNER, 1000)
 
 # Поднятие tap0 и назначение адреса
 subprocess.check_call('ifconfig tap0 10.1.1.8 pointopoint 10.1.1.7 up', shell=True)
+
+
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path.endswith("from_host.docx"):
+            print(f"{event.src_path} has been modified")
+            path_dir = os.path.join(current_dir, 'from_host.docx')
+            read_packet_to_file(path_dir)
 
 
 class bcolors:
@@ -46,77 +50,58 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-class AtomicWrite():
-    def __init__(self, path, mode='w', encoding='utf-8'):
-        self.path = path
-        self.mode = mode if mode == 'wb' else 'w'
-        self.encoding = encoding
-
-    def __enter__(self):
-        self.temp_file = tempfile.NamedTemporaryFile(
-            mode=self.mode,
-            encoding=self.encoding if self.mode != 'wb' else None,
-            delete=False
-        )
-        return self.temp_file
-
-    def __exit__(self, exc_type, exc_message, traceback):
-        self.temp_file.close()
-        if exc_type is None:
-            with open(self.temp_file.name, 'rb') as src, open(self.path, 'wb') as dest:
-                shutil.copyfileobj(src, dest)
-            os.unlink(self.temp_file.name)
-            os.chmod(self.path, 0o664)
-        else:
-            os.unlink(self.temp_file.name)
-            print(bcolors.FAIL + "Break" + bcolors.ENDC)
-
-
 def write_packet_to_file(packet, file_path):
-    global state
-    directory = os.path.dirname(file_path)
-    flag_file = os.path.join(directory, 'flag_to_host.txt')
-    if os.path.exists(flag_file):
-        try:
-            with AtomicWrite(file_path, 'wb') as file:
-                for x in packet:
-                    file.write(bytes([x]))
-                # Вывод содержимого массива пакетов после операции записи
-                print(bcolors.WARNING + "raw_write_data:" + bcolors.ENDC,
-                      ''.join('{:02x} '.format(x) for x in packet))
-                state = 3
-        except:
-            print(bcolors.FAIL + "Failed to write data" + bcolors.ENDC)
+    global temp_write, state
+    with open(file_path, 'wb') as file:
+        if temp_write != packet:
+            temp_write = packet
+            file.write(packet)
+            print(bcolors.WARNING + "raw_write_data:" + bcolors.ENDC, ''.join('{:02x} '.format(x) for x in packet))
+            state = 3
     return state
 
 
 def read_packet_to_file(path_dir):
-    directory = os.path.dirname(path_dir)
-    flag_file = os.path.join(directory, 'flag_to_vb.txt')
-    if os.path.exists(flag_file):
-        with open(path_dir, 'rb') as file:
-            to_TCP = file.read()
+    global temp_read
+    with open(path_dir, 'rb') as file:
+        to_TCP = file.read()
+        if temp_read != to_TCP:
+            temp_read = to_TCP
             print(bcolors.OKGREEN + "raw_read_data:" + bcolors.ENDC, ''.join('{:02x} '.format(x) for x in to_TCP))
-        os.remove(flag_file)
-        return to_TCP
+    return to_TCP
 
 
-while True:
-    print(state)
-    if state == 1:
-        from_TCP = array('B', os.read(tun.fileno(), 2048))
-        if from_TCP:
-            state = 2
-    if state == 2:
-        state = write_packet_to_file(from_TCP, path_dir)
-    if state == 3:
-        to_TCP = read_packet_to_file(path_dir)
-        print(to_TCP)
-        if to_TCP:
-            state =4
-    if state == 4:
-        os.write(tun.fileno(), bytes(to_TCP))
-        state = 1
-
+if __name__ == "__main__":
+    event_handler = MyHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path=current_dir, recursive=False)
+    observer.start()
+    try:
+        while True:
+            print('1',state)
+            if state == 1:
+                from_TCP = array('B', os.read(tun.fileno(), 2048))
+                if from_TCP:
+                    state = 2
+            print('2',state)
+            if state == 2:
+                path_dir = os.path.join(current_dir, 'from_virtual.docx')
+                state = write_packet_to_file(from_TCP, path_dir)
+            print('3',state)
+            if state == 3:
+                path_dir = os.path.join(current_dir, 'from_host.docx')
+                to_TCP = read_packet_to_file(path_dir)
+                if to_TCP:
+                    state = 4
+                else:
+                    state = 1
+            print('4',state)
+            if state == 4:
+                os.write(tun.fileno(), bytes(to_TCP))
+                state = 1
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 tun.close()
--- server.py
+++ server.py
@@ -1,101 +1,63 @@
-import socket
-import tempfile
 import os
-import subprocess
+from socket import *
+from struct import unpack
 
-try:
-    subprocess.check_call('sudo ip route delete 10.0.0.0/8 dev tap0 proto kernel scope link src 10.1.1.8', shell=True)
-except:
-    pass
-# Server configuration
-server_ip = '10.1.1.8'
-server_port = 5050
+ip_address = '10.1.1.8'
+TCP_port = 5050
+class ServerProtocol:
 
-# Create a socket object
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def __init__(self):
+        self.socket = None
+        self.output_dir = '.'
+        self.file_num = 1
 
-# Bind the socket to the server address and port
-server_socket.bind((server_ip, server_port))
+    def listen(self, server_ip, server_port):
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.socket.bind((server_ip, server_port))
+        self.socket.listen(1)
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    def handle_images(self):
 
-class AtomicWrite():
-    def __init__(self, path, mode='w', encoding='utf-8'):
-        self.path = path
-        self.mode = mode if mode == 'wb' else 'w'
-        self.encoding = encoding
-
-    def __enter__(self):
-        self.temp_file = tempfile.NamedTemporaryFile(
-            mode=self.mode,
-            encoding=self.encoding if self.mode != 'wb' else None,
-            delete=False
-        )
-        return self.temp_file
-
-    def __exit__(self, exc_type, exc_message, traceback):
-        self.temp_file.close()
-        if exc_type is None:
-            os.rename(self.temp_file.name, self.path)
-            os.chmod(self.path, 0o664)
-        else:
-            os.unlink(self.temp_file.name)
-            print("Break")
-
-
-while True:
-    # Listen for incoming connections
-    server_socket.listen(5)
-    print(bcolors.OKCYAN + f"Server is listening on {server_ip}:{server_port}..." + bcolors.ENDC)
-
-    client_socket, client_address = server_socket.accept()
-    print(bcolors.WARNING + f"Connection established with {client_address}" + bcolors.ENDC)
-
-    while True:
         try:
-            data = b''
             while True:
+                (connection, addr) = self.socket.accept()
                 try:
-                    chunk = client_socket.recv(1756)
-                    if not chunk:
-                        print("No more data to receive. Client disconnected.")
-                        break
-                    data += chunk
-                    print(f"Received data chunk from client")
-                except socket.timeout:
-                    print("Socket timed out. No more data to receive.")
-                    break
+                    print('Input data')
+                    bs = connection.recv(8)
+                    (length,) = unpack('>Q', bs)
+                    data = b''
+                    while len(data) < length:
+                        to_read = length - len(data)
+                        data += connection.recv(
+                            4096 if to_read > 4096 else to_read)
+                    print(f'Receive data : {data}')
 
-            if data:
-                byte_data = bytes.fromhex(data.decode('utf-8'))
-                print(len(byte_data))
-                print(f'Writing data ... {byte_data}')
-                with AtomicWrite('logo_rec.png', 'wb') as file:
-                    for x in byte_data:
-                        file.write(bytes([x]))
-                print(bcolors.WARNING + "File saved successfully." + bcolors.ENDC)
+                    # send our 0 ack
+                    assert len(b'\00') == 1
+                    connection.sendall(b'\00')
+                finally:
+                    connection.shutdown(SHUT_WR)
+                    connection.close()
 
-                # Send a response back to the client
-                response = "Data received by the server."
-                client_socket.sendall(response.encode())
-            else:
-                print("Empty data received. Closing connection.")
-                break
-        except Exception as e:
-            print(f"Error receiving/sending data: {e}")
-            break
+                with open(os.path.join(
+                        self.output_dir, 'Copy_' + '%02d.png' % self.file_num), 'wb'
+                ) as fp:
+                    fp.write(data)
+                    print('File write success')
 
-    # Close the client socket
-    client_socket.close()
+                self.file_num += 1
+        finally:
+            self.close()
 
-# Close the server socket
-server_socket.close()
+    def close(self):
+        self.socket.close()
+        self.socket = None
+
+
+if __name__ == '__main__':
+    print('START SERVER')
+
+    sp = ServerProtocol()
+    sp.listen(ip_address, TCP_port)
+    print(f'Server listen {ip_address, TCP_port}')
+    sp.handle_images()
